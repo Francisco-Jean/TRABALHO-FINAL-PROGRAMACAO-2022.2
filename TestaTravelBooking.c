f12d9f575a5be0bf9350bbf92db8f516cef19da5
KauanSoares <66488491+KauanSoaress@users.noreply.github.com>
2022-11-18 13:22:02 +0000

    testa_cadastrarPassageiro_dados_nulos implementado

--- TestaTravelBooking.c
+++ TestaTravelBooking.c
@@ -9,8 +9,6 @@ void testa_cadastrarPassageiro_dados_validos(Passageiro *passageiro) {
   
   passageiroAcessa(passageiro, id, nome, endereco);
 
-  Passageiro *auxiliar = lista_passageiro->primeiro;
-
   if ((id == 1) && strcmp(nome, "Jean") && strcmp(endereco, "Vincente Pinzon")) {
     printf("[PASSOU] testa_cadastrarPassageiro_dados_validos.");
   } else {
@@ -26,14 +24,25 @@ void testa_cadastrarPassageiro_dados_invalidos(Passageiro *passageiro) {
   }
 }
 
+void testa_cadastrarPassageiro_dados_nulos(Passageiro *passageiro) {
+  if (passageiro == NULL) {
+    printf("[PASSOU] testa_cadastrarPassageiro_dados_nulos.");
+  } else {
+    printf("[FALHOU] testa_cadastrarPassageiro_dados_nulos.");
+  }
+  return pontuacao;
+}
+
 int main(void) {
   Passageiro *passageiro1 = criarPassageiro(1, "Jean", "Vincente Pinzon");
   Passageiro *passageiro2 = criarPassageiro(2, "Kauan", "Aracati");
   Passageiro *passageiro3 = criarPassageiro(1, "Gustavo", "Trairi");
+  Passageiro *passageiro4 = criarPassageiro(-1, NULL, NULL);
 
   printf("=-=-=-=-=-=-=-=-=-=-= BATERIA DE TESTES 1 =-=-=-=-=-=-=-=-=-=-=");
   testa_cadastrarPassageiro_dados_validos(passageiro1);
   printf("=-=-=-=-=-=-=-=-=-=-= BATERIA DE TESTES 2 =-=-=-=-=-=-=-=-=-=-=");
   testa_cadastrarPassageiro_dados_invalidos(passageiro3);
-  
+  printf("=-=-=-=-=-=-=-=-=-=-= BATERIA DE TESTES 3 =-=-=-=-=-=-=-=-=-=-=");
+  testa_cadastrarPassageiro_dados_nulos(passageiro4);
 }
